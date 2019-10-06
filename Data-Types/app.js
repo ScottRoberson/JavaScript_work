0a9344245b36ddd724d7b4be4cc0ef31462adc92
Scott Roberson <me@gmail.com>
2019-10-06 15:27:43 -0500

    Worked on Data types in JS

---
+++ Data-Types/app.js
@@ -0,0 +1,27 @@
+//Data types in JS
+//Primitive types
+
+//String
+const name= 'John Doe';
+//Number
+const age ='45';
+//Boolean
+const hasKids= true;
+//Null
+const car = null;
+//Undefined
+let test;
+//Symbol
+const sym=Symbol();
+
+//Reference Types
+//Array
+const hobbies= ['movies','music'];
+//Object literal 
+const address ={
+  city: 'Boston',
+  state:'MA'
+}
+const today = new Date();
+console.log(today);
+console.log(typeof address);
