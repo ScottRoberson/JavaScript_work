3aeabfbdb1bbe8671177dc6d3fcebc72de6f4f8a
Scott Roberson <srkazan23@gmail.com>
2019-10-25 21:24:56 -0500

    Event Delegation and Bubbling

---
+++ DOM-selectors/event-bubbling-delegation/app.js
@@ -0,0 +1,37 @@
+//Event Bubbling
+
+document.querySelector('.card-title').addEventListener('click',function(){
+  console.log('card-title');
+})
+
+document.querySelector('.card-content').addEventListener('click',function(){
+  console.log('card-content');
+});
+
+document.querySelector('.card').addEventListener('click',function(){
+  console.log('card');
+})
+
+document.querySelector('.col').addEventListener('click',function(){
+  console.log('col');
+})
+
+//Event Delegation
+const delItem = document.querySelector('.delete-item');
+
+delItem.addEventListener('click',deleteItem);
+
+document.body.addEventListener('click',deleteItem);
+
+function deleteItem(e){
+  //Less Effcient will not work if one element has an added classname
+  if(e.target.parentElement.className === 'delete-item secondary-content'){
+    console.log('delete item');
+  }
+
+  //Preferred Method Get the parent of the parent to remove from list
+  if(e.target.parentElement.classList.contains('delete-item')){
+    console.log('delete item');
+    e.target.parentElement.parentElement.remove();
+  }
+}
